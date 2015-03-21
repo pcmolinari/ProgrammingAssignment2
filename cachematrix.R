841ca63ade56a99fb52ce3aeeb6f6e74506bf457
NLPMO0 <pcmolinari@hotmail.com>
2015-03-21 09:33:54 +0100

    chages made at 9:33 editing of function by sutden Pablo

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
+## Function will return the inverse of a matrix
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+
+
+makeCacheMatrix <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
+#Function will create a vector
+> makeVector <- function(x = numeric()) {
+        m <- NULL
+        print(environment())
+        evn <- environment()
+        print(parent.env(evn))
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        getevn<- function() environment()
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean,
+             getevn = getevn)
+ }
